139e360ced7d8748a0ddce5ac02a891c3a66fa5d
Jialin <Jialin.Ouyang@gmail.com>
2016-07-27 00:15:41 -0700

    ModularUtils support add and multiply

---
+++ numbertheory/ModularUtils.java
@@ -0,0 +1,28 @@
+package template.numbertheory;
+
+public class ModularUtils {
+
+  private static final int MOD = 1000000007;
+
+  public static int add(int a, int b) {
+    return add(a, b, MOD);
+  }
+
+  public static int add(int a, int b, int mod) {
+    a += b;
+    return a >= mod ? a - mod : a;
+  }
+
+  public static int mul(int a, int b) {
+    return mul(a, b, MOD);
+  }
+
+  public static int mul(int a, int b, int mod) {
+    return a > 0
+        ? (
+            b < mod / a
+                ? a * b
+                : (int) ((long) a * b % mod))
+        : 0;
+  }
+}
