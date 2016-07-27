5733c8339f61fd9b12fddb5dc8e3abf3cbd5c49c
Jialin <Jialin.Ouyang@gmail.com>
2016-07-27 00:34:16 -0700

    Bfs ordering of a graph

---
+++ graph/order/BfsOrder.java
@@ -0,0 +1,31 @@
+package template.graph.order;
+
+import template.graph.DirectedGraph;
+
+public class BfsOrder {
+
+  /**
+   * Calculates BFS order starting from {@code startIndex} of the given {@link DirectedGraph}.
+   */
+  public static int[] calc(DirectedGraph<?> graph, int startIndex) {
+    int n = graph.vertexCount;
+    int[] res = new int[n];
+    boolean[] visited = new boolean[n];
+    visited[startIndex] = true;
+    int vertexIndex = 0;
+    res[vertexIndex++] = startIndex;
+    for (int i = 0; i < vertexIndex; ++i) {
+      int u = res[i];
+      for (int v : graph.outgoingVertexIndex(u)) {
+        if (!visited[v]) {
+          visited[v] = true;
+          res[vertexIndex++] = v;
+        }
+      }
+    }
+    if (vertexIndex != n) {
+      throw new IllegalArgumentException("Given directed graph should be connected.");
+    }
+    return res;
+  }
+}
