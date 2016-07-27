b1bb1931ed769c7f69ec8d58bcf650de3e546479
Jialin <Jialin.Ouyang@gmail.com>
2016-07-27 00:55:45 -0700

    ModularUtils add comments

--- numbertheory/ModularUtils.java
+++ numbertheory/ModularUtils.java
@@ -4,19 +4,31 @@ public class ModularUtils {
 
   private static final int MOD = 1000000007;
 
+  /**
+   * Calculates {@code (a + b) % MOD}.
+   */
   public static int add(int a, int b) {
     return add(a, b, MOD);
   }
 
+  /**
+   * Calculates {@code (a + b) % mod}.
+   */
   public static int add(int a, int b, int mod) {
     a += b;
     return a >= mod ? a - mod : a;
   }
 
+  /**
+   * Calculates {@code a * b % MOD}.
+   */
   public static int mul(int a, int b) {
     return mul(a, b, MOD);
   }
 
+  /**
+   * Calculates {@code a * b % mod}.
+   */
   public static int mul(int a, int b, int mod) {
     return a > 0
         ? (
