ccfd3130921b0693ddf9844db4d293f76cf88460
Jialin <Jialin.Ouyang@gmail.com>
2016-09-10 22:07:46 -0700

    Add swap and kth method in ArrayUtils

--- array/IntArrayUtils.java
+++ array/IntArrayUtils.java
@@ -1,6 +1,9 @@
 package template.array;
 
+import java.util.Random;
+
 public class IntArrayUtils {
+  private static final Random random = new Random();
 
   public static int min(int[] values) {
     int res = Integer.MAX_VALUE;
@@ -63,5 +66,37 @@ public class IntArrayUtils {
       }
     }
     return res;
+  }
+
+  public static void swap(int[] values, int uIdx, int vIdx) {
+    if (uIdx == vIdx) return;
+    values[uIdx] ^= values[vIdx];
+    values[vIdx] ^= values[uIdx];
+    values[uIdx] ^= values[vIdx];
+  }
+
+  /**
+   * Returns {@code k}-th (0-indexed) smallest value.
+   */
+  public static int kth(int[] values, int kth) {
+    return kthDFS(values, 0, values.length - 1, kth);
+  }
+
+  private static int kthDFS(int[] values, int lower, int upper, int kth) {
+    if (lower == upper) return values[lower];
+    if (lower + 1 == upper) {
+      if (values[lower] > values[lower + 1]) swap(values, lower, lower + 1);
+      return kth > 0 ? values[lower + 1] : values[lower];
+    }
+    int pivot = values[lower + random.nextInt(upper - lower)];
+    int i = lower, j = upper;
+    while (i <= j) {
+      for ( ; values[i] < pivot; ++i) {}
+      for ( ; values[j] > pivot; --j) {}
+      if (i <= j) swap(values, i++, j--);
+    }
+    return kth < i - lower
+        ? kthDFS(values, lower, i - 1, kth)
+        : kthDFS(values, i, upper, kth - i + lower);
   }
 }
--- array/LongArrayUtils.java
+++ array/LongArrayUtils.java
@@ -1,6 +1,9 @@
 package template.array;
 
+import java.util.Random;
+
 public class LongArrayUtils {
+  private static final Random random = new Random();
 
   public static long min(long[] values) {
     long res = Long.MAX_VALUE;
@@ -63,5 +66,37 @@ public class LongArrayUtils {
       }
     }
     return res;
+  }
+
+  public static void swap(long[] values, int uIdx, int vIdx) {
+    if (uIdx == vIdx) return;
+    values[uIdx] ^= values[vIdx];
+    values[vIdx] ^= values[uIdx];
+    values[uIdx] ^= values[vIdx];
+  }
+
+  /**
+   * Returns {@code k}-th (0-indexed) smallest value.
+   */
+  public static long kth(long[] values, int kth) {
+    return kthDFS(values, 0, values.length - 1, kth);
+  }
+
+  private static long kthDFS(long[] values, int lower, int upper, int kth) {
+    if (lower == upper) return values[lower];
+    if (lower + 1 == upper) {
+      if (values[lower] > values[lower + 1]) swap(values, lower, lower + 1);
+      return kth > 0 ? values[lower + 1] : values[lower];
+    }
+    long pivot = values[lower + random.nextInt(upper - lower)];
+    int i = lower, j = upper;
+    while (i <= j) {
+      for ( ; values[i] < pivot; ++i) {}
+      for ( ; values[j] > pivot; --j) {}
+      if (i <= j) swap(values, i++, j--);
+    }
+    return kth < i - lower
+        ? kthDFS(values, lower, i - 1, kth)
+        : kthDFS(values, i, upper, kth - i + lower);
   }
 }
