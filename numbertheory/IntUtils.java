4b3642707e3780d814fcba79c0f0e0c0fa296025
Jialin <Jialin.Ouyang@gmail.com>
2016-09-18 21:38:43 -0700

    Add fix method in IntUtils

--- numbertheory/IntUtils.java
+++ numbertheory/IntUtils.java
@@ -4,15 +4,16 @@ import template.array.IntArrayUtils;
 
 public class IntUtils {
 
-  private static final int MOD = 1000000007;
+  public static int MOD = 1000000007;
+
+  private static int[] x = new int[2];
 
   public static int add(int a, int b) {
     return add(a, b, MOD);
   }
 
   public static int add(int a, int b, int mod) {
-    a += b;
-    return a >= mod ? a - mod : a;
+    return slightFix(a + b, mod);
   }
 
   public static int sub(int a, int b) {
@@ -20,8 +21,18 @@ public class IntUtils {
   }
 
   public static int sub(int a, int b, int mod) {
-    a -= b;
-    return a >= 0 ? a + mod : a;
+    return slightFix(a - b, mod);
+  }
+
+  public static int fix(int a) {
+    return fix(a, MOD);
+  }
+
+  public static int fix(int a, int mod) {
+    a = slightFix(a, mod);
+    return 0 <= a && a < mod
+        ? a
+        : slightFix(a % mod, mod);
   }
 
   public static int mul(int a, int b) {
@@ -30,21 +41,31 @@ public class IntUtils {
 
   public static int mul(int a, int b, int mod) {
     return a > 0
-        ? (
-            b < mod / a
-                ? a * b
-                : (int) ((long) a * b % mod))
+        ? (b < mod / a
+            ? a * b
+            : (int) ((long) a * b % mod))
         : 0;
   }
 
-  public static int pow(int a, int b) {
+  public static int pow(int a, long b) {
     return pow(a, b, MOD);
   }
 
-  public static int pow(int a, int b, int mod) {
+  public static int pow(int a, long b, int mod) {
     int res = 1;
-    for ( ; b > 0; b >>= 1, a = mul(a, a, mod)) if ((b & 1) > 0) res = mul(res, a);
+    for ( ; b > 0; b >>= 1, a = mul(a, a, mod)) if ((b & 1) > 0) res = mul(res, a, mod);
     return res;
+  }
+
+  public static int modInverse(int a) {
+    return modInverse(a, MOD);
+  }
+
+  public static int modInverse(int a, int mod) {
+    if (extGcd(a, mod, x) != 1) {
+      throw new IllegalArgumentException();
+    }
+    return slightFix(x[0], mod);
   }
 
   public static int gcd(int a, int b) {
@@ -61,5 +82,13 @@ public class IntUtils {
     IntArrayUtils.swap(x, 0, 1);
     x[1] -= a / b * x[0];
     return gcd;
+  }
+
+  private static int slightFix(int a, int mod) {
+    return a >= mod
+        ? a - mod
+        : a < 0
+            ? a + mod
+            : a;
   }
 }
