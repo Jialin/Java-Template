50e9140c36a23520cfccd235ab63984803478abb
Jialin <Jialin.Ouyang@gmail.com>
2016-10-11 09:57:06 -0700

    Tweak the method in BfsOrder

--- graph/order/BfsOrder.java
+++ graph/order/BfsOrder.java
@@ -4,28 +4,20 @@ import template.graph.DirectedGraph;
 
 public class BfsOrder {
 
-  /**
-   * Calculates BFS order starting from {@code startIndex} of the given {@link DirectedGraph}.
-   */
-  public static int[] calc(DirectedGraph graph, int startIndex) {
-    int n = graph.vertexCnt;
-    int[] res = new int[n];
-    boolean[] visited = new boolean[n];
-    visited[startIndex] = true;
-    int vertexIndex = 0;
-    res[vertexIndex++] = startIndex;
-    for (int i = 0; i < vertexIndex; ++i) {
-      int u = res[i];
+  public static int calc(DirectedGraph graph, int startIdx, int[] bfs, boolean[] visited) {
+    if (visited[startIdx]) return 0;
+    visited[startIdx] = true;
+    int res = 0;
+    bfs[res++] = startIdx;
+    for (int i = 0; i < res; ++i) {
+      int u = bfs[i];
       for (int edgeIdx = graph.lastOut[u]; edgeIdx >= 0; edgeIdx = graph.nextOut[edgeIdx]) {
         int v = graph.toIdx[edgeIdx];
         if (!visited[v]) {
           visited[v] = true;
-          res[vertexIndex++] = v;
+          bfs[res++] = v;
         }
       }
-    }
-    if (vertexIndex != n) {
-      throw new IllegalArgumentException("Given directed graph should be connected.");
     }
     return res;
   }
