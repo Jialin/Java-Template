19ca6f99f0b6146bc8093e467a97b63f915b29fa
Jialin <Jialin.Ouyang@gmail.com>
2016-10-26 00:55:37 -0700

    Implement 1D dancing link

---
+++ collections/dancinglink/DancingLink.java
@@ -0,0 +1,34 @@
+package template.collections.dancinglink;
+
+public class DancingLink {
+
+  public int first;
+  public int[] prev, next;
+
+  public DancingLink(int capacity) {
+    prev = new int[capacity];
+    next = new int[capacity];
+    init(capacity);
+  }
+
+  public void init(int n) {
+    for (int i = 0; i < n; ++i) {
+      prev[i] = i - 1;
+      next[i] = i + 1;
+    }
+    next[n - 1] = -1;
+    first = 0;
+  }
+
+  public void cover(int idx) {
+    int prevIdx = prev[idx], nextIdx = next[idx];
+    if (prevIdx >= 0) {
+      next[prevIdx] = nextIdx;
+    } else {
+      first = nextIdx;
+    }
+    if (nextIdx >= 0) {
+      prev[nextIdx] = prevIdx;
+    }
+  }
+}
