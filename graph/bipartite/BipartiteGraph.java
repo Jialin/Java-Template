79fa537a7e5772f698b328d69f65484c2a785a76
Jialin <Jialin.Ouyang@gmail.com>
2016-12-12 22:25:13 -0800

    Implement BipartiteGraph

---
+++ graph/bipartite/BipartiteGraph.java
@@ -0,0 +1,51 @@
+package template.graph.bipartite;
+
+import template.graph.basic.AbstractDirectedGraph;
+
+public class BipartiteGraph extends AbstractDirectedGraph {
+
+  public int leftCnt, rightCnt;
+
+  public BipartiteGraph(int leftCntCapacity, int rightCntCapacity, int edgeCapacity) {
+    super(leftCntCapacity + rightCntCapacity, edgeCapacity, false);
+    init(leftCntCapacity, rightCntCapacity);
+  }
+
+  @Override
+  public void createSubclass(int vertexCapacity, int edgeCapacity) {}
+
+  @Override
+  public void initSubclass(int vertexCnt) {}
+
+  @Override
+  public void init(int vertexCnt) {
+    throw new UnsupportedOperationException();
+  }
+
+  public void init(int leftCnt, int rightCnt) {
+    this.leftCnt = leftCnt;
+    this.rightCnt = rightCnt;
+    super.init(leftCnt + rightCnt);
+  }
+
+  @Override
+  public void add(int fromIdx, int toIdx) {
+    super.add(fromIdx, leftCnt + toIdx);
+  }
+
+  @Override
+  public int toIdx(int edgeIdx) {
+    return super.toIdx(edgeIdx) - leftCnt;
+  }
+
+  @Override
+  public int lastIn(int nodeIdx) {
+    return super.lastIn(leftCnt + nodeIdx);
+  }
+
+  @Override
+  public int inDegree(int nodeIdx) {
+    return super.inDegree(leftCnt + nodeIdx);
+  }
+}
+
