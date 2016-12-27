d1d32342fab2bc6c1f3136b777ccb5a84ce9f6ba
Jialin <Jialin.Ouyang@gmail.com>
2016-12-26 21:30:11 -0800

    Move all template files under template/ subpackages

--- gen.py
+++ gen.py
@@ -15,17 +15,28 @@ def findTemplates(root):
 ROOT = '/Users/jouyang/IdeaProjects/ACM/src/template/'
 SEPARATOR = '=' * 100
 FILENAME = 'filename'
+PACKAGE = 'template'
 
 for (root, filename) in findTemplates(ROOT):
+    if not root.endswith(PACKAGE):
+        print 'Template file (%s) should live in template packages!' % filename
+        continue
     print 'Processing', filename
     lines = [line.rstrip('\n') for line in open(os.path.join(root, filename))]
     separatorLine = [i for i, line in enumerate(lines) if line == SEPARATOR][0];
+    templateRoot = root[ : -len(PACKAGE)]
     for setting in json.loads(''.join(lines[ : separatorLine])):
+        outputLines = []
+        for line in lines[separatorLine + 1 : ]:
+            parsedLine = line
+            for (tag, value) in setting.items():
+                parsedLine = string.replace(parsedLine, '%' + tag + '%', value)
+            outputLines.append(parsedLine + '\n')
         outputFilename = setting[FILENAME] + '.java'
+        if os.path.exists(templateRoot + outputFilename) and outputLines == open(templateRoot + outputFilename, 'r').readlines():
+            continue
         print '\tGenerating', outputFilename
-        with open(os.path.join(root, outputFilename), 'w') as outputFile:
-            for line in lines[separatorLine + 1 : ]:
-                parsedLine = line
-                for (tag, value) in setting.items():
-                    parsedLine = string.replace(parsedLine, '%' + tag + '%', value)
-                outputFile.write(parsedLine + '\n')
+        with open(os.path.join(templateRoot, outputFilename), 'w') as outputFile:
+            for line in outputLines:
+                outputFile.write(line)
+
