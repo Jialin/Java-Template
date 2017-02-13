f016c53f960eb63cb1bc4e7d1f65fa8b5c5f4832
Jialin <Jialin.Ouyang@gmail.com>
2017-02-12 21:26:36 -0800

    Support hash char array

--- numbertheory/hash/HashUtils.java
+++ numbertheory/hash/HashUtils.java
@@ -6,6 +6,18 @@ public class HashUtils {
   public static long END_SEED = 1000000000000000003L;
   public static int BASE = 1000000007;
 
+  public static long calc(char[] values) {
+    return calc(values, 0, values.length);
+  }
+
+  public static long calc(char[] values, int fromIdx, int toIdx) {
+    long res = START_SEED;
+    for (int i = fromIdx; i < toIdx; ++i) {
+      res = (i == fromIdx ? res : res * BASE) ^ hashSingle(values[i]);
+    }
+    return res * END_SEED;
+  }
+
   public static long calc(long[] values) {
     return calc(values, 0, values.length);
   }
@@ -28,6 +40,10 @@ public class HashUtils {
       res = (i == fromIdx ? res : res * BASE) ^ hashedValues[i];
     }
     return res * END_SEED;
+  }
+
+  private static long hashSingle(char value) {
+    return (START_SEED ^ value) * END_SEED;
   }
 
   private static long hashSingle(long value) {
