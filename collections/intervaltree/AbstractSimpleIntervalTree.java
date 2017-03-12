b27b39e63b9c938f99be413d300011f6a4593879
Jialin <Jialin.Ouyang@gmail.com>
2017-03-12 00:18:04 -0800

    Implement single node update in IntervalTree

--- collections/intervaltree/AbstractSimpleIntervalTree.java
+++ collections/intervaltree/AbstractSimpleIntervalTree.java
@@ -3,7 +3,7 @@ package template.collections.intervaltree;
 import template.collections.list.IntArrayList;
 
 /**
- * Simple and efficient interval tree. (http://codeforces.com/blog/entry/18051?)
+ * Simple and efficient interval tree. (http://codeforces.com/blog/entry/18051)
  */
 public abstract class AbstractSimpleIntervalTree {
 
@@ -13,10 +13,15 @@ public abstract class AbstractSimpleIntervalTree {
 
   public abstract void merge(int nodeIdx, int leftNodeIdx, int rightNodeIdx);
 
+  public abstract void updateNode(int nodeIdx, int lower, int upper);
+
   public abstract void calcAppend(int nodeIdx, int lower, int upper);
 
+  public abstract String toString(int nodeIdx);
+
+  public int[] lower, upper;
+
   private int n;
-  private int[] lower, upper;
   private IntArrayList calcLeftIdx, calcRightIdx;
 
   public AbstractSimpleIntervalTree(int leafCapacity) {
@@ -43,7 +48,35 @@ public abstract class AbstractSimpleIntervalTree {
         upper[i] = upper[right];
         merge(i, left, right);
       } else {
-        lower[i] = -1;
+        lower[i] = n;
+        upper[i] = -1;
+      }
+    }
+  }
+
+  public void updateRange(int lower, int upper) {
+    int l = lower, r = upper;
+    for (lower += n, upper += n; lower < upper; lower >>= 1, upper >>= 1) {
+      if ((lower & 1) > 0) {
+        updateNode(lower, this.lower[lower], this.upper[lower]);
+        ++lower;
+      }
+      if ((upper & 1) > 0) {
+        --upper;
+        updateNode(upper, this.lower[upper], this.upper[upper]);
+      }
+    }
+    for (l = (l + n) >> 1, r = (r + n - 1) >> 1; l > 0 || r > 0; ) {
+      if (l < r) {
+        mergeInternal(r);
+        r >>= 1;
+      } else if (r < l) {
+        mergeInternal(l);
+        l >>= 1;
+      } else {
+        mergeInternal(l);
+        l >>= 1;
+        r >>= 1;
       }
     }
   }
@@ -63,5 +96,21 @@ public abstract class AbstractSimpleIntervalTree {
       int idx = calcRightIdx.get(i);
       calcAppend(idx, this.lower[idx], this.upper[idx]);
     }
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n << 1; ++i) if (lower[i] < upper[i]) {
+      sb.append(String.format("@%d[%d,%d):%s", i, lower[i], upper[i], toString(i)));
+      if (i + 1 < n << 1) sb.append('\n');
+    }
+    return sb.toString();
+  }
+
+  private void mergeInternal(int nodeIdx) {
+    if (lower[nodeIdx] >= upper[nodeIdx]) return;
+    int leftNodeIdx = nodeIdx << 1;
+    merge(nodeIdx, leftNodeIdx, leftNodeIdx | 1);
   }
 }
