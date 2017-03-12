e39a6006b1cf3eccf683e7ab02b6bd05b2c225dd
Jialin <Jialin.Ouyang@gmail.com>
2017-03-12 00:17:36 -0800

    Update Bitset to ensure capacity

--- collections/bitset/BitSet.java
+++ collections/bitset/BitSet.java
@@ -1,32 +1,85 @@
 package template.collections.bitset;
 
+import template.numbertheory.number.IntUtils;
+
 import java.util.Arrays;
 
 public class BitSet {
 
-  private int n, m;
+  public int n;
+
+  private int m;
   private long[] value, tmpValue;
 
-  public BitSet(int n) {
+  public BitSet(int capacity) {
+    init(capacity);
+  }
+
+  public BitSet(BitSet o) {
+    init(o);
+  }
+
+  public void init(int n) {
     this.n = n;
     m = (n + 63) >> 6;
-    value = new long[m];
-    tmpValue = new long[m];
+    ensureCapacity(m);
+    clear();
+  }
+
+  public void init(BitSet o) {
+    init(o.n);
+    for (int i = 0; i < m; ++i) {
+      value[i] = o.value[i];
+    }
   }
 
   public void clear() {
-    Arrays.fill(value, 0);
+    Arrays.fill(value, 0, m, 0);
   }
 
   public void set(int bit) {
     value[bit >> 6] |= 1L << (bit & 63);
   }
 
+  public void clear(int bit) {
+    value[bit >> 6] &= ~(1L << (bit & 63));
+  }
+
+  public void set(int bit, boolean value) {
+    if (value) {
+      set(bit);
+    } else {
+      clear(bit);
+    }
+  }
+
   public boolean get(int bit) {
     return ((value[bit >> 6] >>> (bit & 63)) & 1) > 0;
   }
 
-  /** this << shift */
+  public int cardinality() {
+    int res = 0;
+    for (int i = 0; i < m; ++i) {
+      res += Long.bitCount(value[i]);
+    }
+    return res;
+  }
+
+  public boolean intersects(BitSet o) {
+    for (int i = 0; i < m; ++i) {
+      if ((value[i] & o.value[i]) != 0) return true;
+    }
+    return false;
+  }
+
+  /** this |= o */
+  public void or(BitSet o) {
+    for (int i = 0; i < m; ++i) {
+      value[i] |= o.value[i];
+    }
+  }
+
+  /** this <<= shift */
   public void shiftLeft(int shift) {
     shiftLeftInternal(shift);
     for (int i = 0; i < m; ++i) {
@@ -34,7 +87,7 @@ public class BitSet {
     }
   }
 
-  /** this | (this << shift) */
+  /** this |= this << shift */
   public void orShiftLeft(int shift) {
     shiftLeftInternal(shift);
     for (int i = 0; i < m; ++i) {
@@ -45,7 +98,7 @@ public class BitSet {
   @Override
   public String toString() {
     StringBuffer sb = new StringBuffer(n);
-    for (int i = n - 1; i >= 0; --i) {
+    for (int i = 0; i < n; ++i) {
       sb.append(get(i) ? '1' : '0');
     }
     return sb.toString();
@@ -61,5 +114,12 @@ public class BitSet {
       }
       tmpValue[i + x] |= value[i] << y;
     }
+  }
+
+  private void ensureCapacity(int capacity) {
+    if (value != null && value.length >= capacity) return;
+    int n = IntUtils.nextPow2(capacity);
+    value = new long[n];
+    tmpValue = new long[n];
   }
 }
