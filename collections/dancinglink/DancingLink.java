f252d970e21697e2a7009d360de2d5fa536be876
Jialin <Jialin.Ouyang@gmail.com>
2017-10-05 10:53:48 -0700

    Lazily construct the storage in DancingLink

--- collections/dancinglink/DancingLink.java
+++ collections/dancinglink/DancingLink.java
@@ -1,17 +1,31 @@
 package template.collections.dancinglink;
 
+import template.numbertheory.number.IntUtils;
+
+import java.util.Arrays;
+import java.util.function.IntConsumer;
+
 public class DancingLink {
+
+  private boolean[] covered;
 
   public int first;
   public int[] prev, next;
 
+  public DancingLink() {}
+
   public DancingLink(int capacity) {
-    prev = new int[capacity];
-    next = new int[capacity];
-    init(capacity);
+    this(capacity, true);
+  }
+
+  public DancingLink(int capacity, boolean initialize) {
+    ensureCapacity(capacity);
+    if (initialize) init(capacity);
   }
 
   public void init(int n) {
+    ensureCapacity(n);
+    Arrays.fill(covered, 0, n, false);
     for (int i = 0; i < n; ++i) {
       prev[i] = i - 1;
       next[i] = i + 1;
@@ -20,7 +34,9 @@ public class DancingLink {
     first = 0;
   }
 
-  public void cover(int idx) {
+  public boolean cover(int idx) {
+    if (covered[idx]) return false;
+    covered[idx] = true;
     int prevIdx = prev[idx], nextIdx = next[idx];
     if (prevIdx >= 0) {
       next[prevIdx] = nextIdx;
@@ -30,6 +46,11 @@ public class DancingLink {
     if (nextIdx >= 0) {
       prev[nextIdx] = prevIdx;
     }
+    return true;
+  }
+
+  public boolean isCovered(int idx) {
+    return covered[idx];
   }
 
   // TODO: verify
@@ -43,5 +64,19 @@ public class DancingLink {
     if (nextIdx >= 0) {
       prev[nextIdx] = idx;
     }
+  }
+
+  public void forEach(IntConsumer consumer) {
+    for (int x = first; x >= 0; x = next[x]) {
+      consumer.accept(x);
+    }
+  }
+
+  private void ensureCapacity(int capacity) {
+    if (prev != null && prev.length >= capacity) return;
+    int size = IntUtils.nextPow2(capacity);
+    prev = new int[size];
+    next = new int[size];
+    covered = new boolean[size];
   }
 }
