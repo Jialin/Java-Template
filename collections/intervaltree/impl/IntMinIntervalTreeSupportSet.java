95112803bed7e80e0ac7794172febc73bd8c8c0f
Jialin <Jialin.Ouyang@gmail.com>
2017-10-06 15:16:44 -0700

    Remove AbstractSimpleIntervalTree

---
+++ collections/intervaltree/impl/IntMinIntervalTreeSupportSet.java
@@ -0,0 +1,92 @@
+package template.collections.intervaltree.impl;
+
+import template.collections.intervaltree.AbstractIntervalTreeWithLazyPropagation;
+
+public class IntMinIntervalTreeSupportSet extends AbstractIntervalTreeWithLazyPropagation {
+
+  public IntMinIntervalTreeSupportSet(int leafCapacity) {
+    super(leafCapacity);
+  }
+
+  public IntMinIntervalTreeSupportSet(int leafCapacity, boolean initialize) {
+    super(leafCapacity, initialize);
+  }
+
+  int[] minValue;
+  boolean[] toSet;
+  int[] setValue;
+
+  @Override
+  public void createStorage(int nodeCapacity) {
+    minValue = new int[nodeCapacity];
+    toSet = new boolean[nodeCapacity];
+    setValue = new int[nodeCapacity];
+  }
+
+  @Override
+  public void initLeaf(int idxInTree, int idx) {
+    minValue[idxInTree] = initValues[idx];
+  }
+
+  @Override
+  public void assignFakeLazyPropagation() {
+    toSet[0] = true;
+    setValue[0] = updateValue;
+  }
+
+  @Override
+  public void pushLazyPropagation(int fromIdxInTree, int toIdxInTree) {
+    if (toSet[fromIdxInTree]) {
+      toSet[toIdxInTree] = true;
+      setValue[toIdxInTree] = setValue[fromIdxInTree];
+      minValue[toIdxInTree] = setValue[toIdxInTree];
+    }
+  }
+
+  @Override
+  public void clearLazyPropagation(int idxInTree) {
+    toSet[idxInTree] = false;
+  }
+
+  @Override
+  public void merge(int leftIdxInTree, int rightIdxInTree, int idxInTree) {
+    minValue[idxInTree] = Math.min(minValue[leftIdxInTree], minValue[rightIdxInTree]);
+  }
+
+  @Override
+  public void copyForCalc(int fromIdxInTree, int toIdxInTree) {
+    minValue[toIdxInTree] = minValue[fromIdxInTree];
+  }
+
+  @Override
+  public void clearNodeForCalc(int idxInTree) {
+    minValue[idxInTree] = Integer.MAX_VALUE;
+  }
+
+  @Override
+  public String toDisplay(int idxInTree) {
+    return String.format(
+        "min:%d set:%s",
+        minValue[idxInTree],
+        toSet[idxInTree] ? String.valueOf(setValue[idxInTree]) : "X");
+  }
+
+  int[] initValues;
+
+  public void init(int n, int[] initValues) {
+    this.initValues = initValues;
+    super.init(n);
+  }
+
+  int updateValue;
+
+  public void update(int lower, int upper, int updateValue) {
+    this.updateValue = updateValue;
+    updateRange(lower, upper);
+  }
+
+  public int calc(int lower, int upper) {
+    calcRange(lower, upper);
+    return minValue[0];
+  }
+}
