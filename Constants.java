e7c0981c5ed631f94eb277fbb9704b6bcb7c31d2
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2018-11-25 08:26:37 -0800

    Constants

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package template;
+
+public class Constants {
+
+  /** North - East - South - West */
+  public static final int[] DX4 = {-1, 0, 1,  0};
+  public static final int[] DY4 = { 0, 1, 0, -1};
+}
