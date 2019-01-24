3ac4aee73c43d14344344f4b222da481c74c297a
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-01-24 03:29:32 -0800

    simplify DancingLink

--- collections/dancinglink/DancingLink.java
+++ collections/dancinglink/DancingLink.java
@@ -1,18 +1,16 @@
 package template.collections.dancinglink;
 
+import template.io.Displayable;
 import template.numbertheory.number.IntUtils;
 
 import java.util.Arrays;
 import java.util.function.IntConsumer;
 
-public class DancingLink {
+public class DancingLink implements Displayable {
 
+  private int n;
   private boolean[] covered;
-
-  public int first;
-  public int[] prev, next;
-
-  public DancingLink() {}
+  private int[] prev, next;
 
   public DancingLink(int capacity) {
     this(capacity, true);
@@ -20,63 +18,85 @@ public class DancingLink {
 
   public DancingLink(int capacity, boolean initialize) {
     ensureCapacity(capacity);
-    if (initialize) init(capacity);
+    if (initialize) {
+      init(capacity);
+    }
   }
 
   public void init(int n) {
+    this.n = n;
     ensureCapacity(n);
     Arrays.fill(covered, 0, n, false);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n + 2; ++i) {
       prev[i] = i - 1;
       next[i] = i + 1;
     }
-    next[n - 1] = -1;
-    first = 0;
   }
 
   public boolean cover(int idx) {
-    if (covered[idx]) return false;
+    if (covered[idx]) {
+      return false;
+    }
     covered[idx] = true;
-    int prevIdx = prev[idx], nextIdx = next[idx];
-    if (prevIdx >= 0) {
-      next[prevIdx] = nextIdx;
-    } else {
-      first = nextIdx;
-    }
-    if (nextIdx >= 0) {
-      prev[nextIdx] = prevIdx;
-    }
+    ++idx;
+    int prevIdx = prev[idx];
+    int nextIdx = next[idx];
+    next[prevIdx] = nextIdx;
+    prev[nextIdx] = prevIdx;
     return true;
   }
 
-  public boolean isCovered(int idx) {
+  public boolean covered(int idx) {
     return covered[idx];
   }
 
-  // TODO: verify
-  public void uncover(int idx) {
-    int prevIdx = prev[idx], nextIdx = next[idx];
-    if (prevIdx >= 0) {
-      next[prevIdx] = idx;
-    } else {
-      first = idx;
+  public boolean uncover(int idx) {
+    if (!covered[idx]) {
+      return false;
     }
-    if (nextIdx >= 0) {
-      prev[nextIdx] = idx;
-    }
+    covered[idx] = false;
+    ++idx;
+    int prevIdx = prev[idx];
+    int nextIdx = next[idx];
+    next[prevIdx] = idx;
+    prev[nextIdx] = idx;
+    return true;
   }
 
+  public int first() {
+    return next[0] - 1;
+  }
+
+  public int next(int idx) {
+    return next[idx + 1] - 1;
+  }
+
+  // TODO(jouyang): to be verify
   public void forEach(IntConsumer consumer) {
-    for (int x = first; x >= 0; x = next[x]) {
+    for (int x = first(); x < n; x = next(x)) {
       consumer.accept(x);
     }
   }
 
   private void ensureCapacity(int capacity) {
-    if (prev != null && prev.length >= capacity) return;
-    int size = IntUtils.nextPow2(capacity);
+    if (prev != null && prev.length >= capacity + 2) {
+      return;
+    }
+    int size = IntUtils.nextPow2(capacity + 2);
     prev = new int[size];
     next = new int[size];
     covered = new boolean[size];
+  }
+
+  @Override
+  public String toDisplay() {
+    final StringBuilder sb = new StringBuilder("[");
+    forEach(x -> {
+      if (x != first()) {
+        sb.append(',');
+      }
+      sb.append(x);
+    });
+    return sb.append(']').toString();
   }
 }
