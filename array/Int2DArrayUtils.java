3727c5a15f32549da4af27b9f3d9288475628b3f
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-01-25 14:57:51 -0800

    2DArrayUtils#diagonalFlip

--- array/Int2DArrayUtils.java
+++ array/Int2DArrayUtils.java
@@ -1,10 +1,10 @@
 package template.array;
 
-public class Char2DArrayUtils {
+public class Int2DArrayUtils {
 
-  public static void rotate90(int n, char[][] values) {
+  public static void rotate90(int n, int[][] values) {
     for (int i = (n - 1) >> 1; i >= 0; --i) for (int j = (n >> 1) - 1; j >= 0; --j) {
-      char tmp = values[i][j];
+      int tmp = values[i][j];
       values[i][j] = values[n - 1 - j][i];
       values[n - 1 - j][i] = values[n - 1 - i][n - 1 - j];
       values[n - 1 - i][n - 1 - j] = values[j][n - 1 - i];
@@ -12,7 +12,7 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static void diagonalFlip(int n, int m, char[][] values) {
+  public static void diagonalFlip(int n, int m, int[][] values) {
     if (n >= m) {
       for (int i = 0; i < n; ++i) for (int j = Math.min(i, m) - 1; j >= 0; --j) {
         swap(values, i, j, j, i);
@@ -24,7 +24,7 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static void swap(char[][] values, int x1, int y1, int x2, int y2) {
+  public static void swap(int[][] values, int x1, int y1, int x2, int y2) {
     if (x1 != x2 || y1 != y2) {
       values[x1][y1] ^= values[x2][y2];
       values[x2][y2] ^= values[x1][y1];
@@ -32,17 +32,17 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static boolean equals(int n, char[][] values1, char[][] values2) {
+  public static boolean equals(int n, int[][] values1, int[][] values2) {
     for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) if (values1[i][j] != values2[i][j]) {
       return false;
     }
     return true;
   }
 
-  public static String toDisplay(int n, int m, char[][] values) {
+  public static String toDisplay(int n, int m, int[][] values) {
     StringBuilder sb = new StringBuilder("[\n");
     for (int i = 0; i < n; ++i) {
-      sb.append(String.format("\t%s,\n", CharArrayUtils.toDisplay(values[i], 0, m)));
+      sb.append(String.format("\t%s,\n", IntArrayUtils.toDisplay(values[i], 0, m)));
     }
     return sb.append(']').toString();
   }
--- array/Long2DArrayUtils.java
+++ array/Long2DArrayUtils.java
@@ -1,10 +1,10 @@
 package template.array;
 
-public class Char2DArrayUtils {
+public class Long2DArrayUtils {
 
-  public static void rotate90(int n, char[][] values) {
+  public static void rotate90(int n, long[][] values) {
     for (int i = (n - 1) >> 1; i >= 0; --i) for (int j = (n >> 1) - 1; j >= 0; --j) {
-      char tmp = values[i][j];
+      long tmp = values[i][j];
       values[i][j] = values[n - 1 - j][i];
       values[n - 1 - j][i] = values[n - 1 - i][n - 1 - j];
       values[n - 1 - i][n - 1 - j] = values[j][n - 1 - i];
@@ -12,7 +12,7 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static void diagonalFlip(int n, int m, char[][] values) {
+  public static void diagonalFlip(int n, int m, long[][] values) {
     if (n >= m) {
       for (int i = 0; i < n; ++i) for (int j = Math.min(i, m) - 1; j >= 0; --j) {
         swap(values, i, j, j, i);
@@ -24,7 +24,7 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static void swap(char[][] values, int x1, int y1, int x2, int y2) {
+  public static void swap(long[][] values, int x1, int y1, int x2, int y2) {
     if (x1 != x2 || y1 != y2) {
       values[x1][y1] ^= values[x2][y2];
       values[x2][y2] ^= values[x1][y1];
@@ -32,17 +32,17 @@ public class Char2DArrayUtils {
     }
   }
 
-  public static boolean equals(int n, char[][] values1, char[][] values2) {
+  public static boolean equals(int n, long[][] values1, long[][] values2) {
     for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) if (values1[i][j] != values2[i][j]) {
       return false;
     }
     return true;
   }
 
-  public static String toDisplay(int n, int m, char[][] values) {
+  public static String toDisplay(int n, int m, long[][] values) {
     StringBuilder sb = new StringBuilder("[\n");
     for (int i = 0; i < n; ++i) {
-      sb.append(String.format("\t%s,\n", CharArrayUtils.toDisplay(values[i], 0, m)));
+      sb.append(String.format("\t%s,\n", LongArrayUtils.toDisplay(values[i], 0, m)));
     }
     return sb.append(']').toString();
   }
